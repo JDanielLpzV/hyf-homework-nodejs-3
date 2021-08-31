851eca88b6f8772f6402124477998470bbfca60e
JDanielLpzV <juandan.lopezvlqz.95@gmail.com>
2021-08-30 21:27:59 -0600

    Add routes GET & POST

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+let users =[];
+
 
 app.get('/',(req,res)=>{
     res.send("Hello World!");
 });
+app.get('/users',(req,res)=>{
+    res.send(users);
+});
+app.get('/user/:id',(req,res)=>{
+    res.json(users[Number(req.params.id)]);
+});
+
+app.post('/user',(req,res)=>{
+    let id=0;
+    if(id!==null){
+        users.push({id:id});
+        res.json(users[id]);
+    }else{
+        res.end('User not added');
+    }
+});
+
 
 
 
